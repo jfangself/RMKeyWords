c097fb32d4e72e2b88dac36bcedd20757cbbd3e3
joeyfang-owmessaging <joey.fang@owmessaging.com>
2016-04-08 11:18:07 +0800

    Selenium Grid with node 10.13.16.198, Firefox

--- src/rm/testScript/HelloWorld.java
+++ src/rm/testScript/HelloWorld.java
@@ -2,13 +2,20 @@ package rm.testScript;
 
 import static rm.util.WaitUitl.sleep;
 import static rm.util.WaitUitl.waitWebElement;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import junit.framework.Assert;
 
 import org.apache.log4j.xml.DOMConfigurator;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -17,6 +24,8 @@ public class HelloWorld {
 
 	WebDriver driver;
 	String baseUrl = "https://www.sogou.com";
+	// define node url
+	String nodeUrl = "http://10.13.16.198:6655/wd/hub";
 
 	@Test
 	public void helloWorld() {
@@ -39,12 +48,25 @@ public class HelloWorld {
 	}
 
 	@BeforeClass
-	public void beforeClass() {
+	public void beforeClass() throws MalformedURLException {
+
+		// DesiredCapabilities capability =
+		// DesiredCapabilities.internetExplorer();
+		// capability.setBrowserName("internetExplorer");
+		// capability.setPlatform(Platform.WINDOWS);
+
+		DesiredCapabilities capability = DesiredCapabilities.firefox();
+		capability.setBrowserName("firefox");
+		capability.setPlatform(Platform.WINDOWS);
 
 		DOMConfigurator.configure("Log4j.xml");
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
-		driver = new ChromeDriver();
+		/*
+		 * System.setProperty("webdriver.chrome.driver",
+		 * "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe"
+		 * ); driver = new ChromeDriver();
+		 */
+
+		driver = new RemoteWebDriver(new URL(nodeUrl), capability);
 
 	}
 
